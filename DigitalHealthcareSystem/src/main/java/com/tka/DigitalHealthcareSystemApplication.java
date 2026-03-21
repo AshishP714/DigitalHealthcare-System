6d56522bcd172ff29c350705b573148fd0464a97
AshishP714 <ashu.pardeshi714@gmail.com>
2026-03-21 22:44:44 +0530

    return

--- DigitalHealthcareSystem/src/main/java/com/tka/DigitalHealthcareSystemApplication.java
+++ DigitalHealthcareSystem/src/main/java/com/tka/DigitalHealthcareSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DigitalHealthcareSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DigitalHealthcareSystemApplication.class, args);	
+		SpringApplication.run(DigitalHealthcareSystemApplication.class, args);
+		System.err.println("Application started...");	
 	}
 }
